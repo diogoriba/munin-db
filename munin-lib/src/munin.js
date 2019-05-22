f9f90886a7c0ce0f645950ca3fc8b7143efd6edd
Diogo Ribeiro <diogoriba@gmail.com>
2019-05-22 19:32:27 -0300

    making existing functions adherent to redis interfaces

--- munin-lib/src/munin.js
+++ munin-lib/src/munin.js
@@ -1,27 +1,42 @@
 const errors = require('./munin-errors.js');
 
-const muninLib = (function muninLib() {
+const Munin = (function muninLib() {
     const ctor = function() {
         this.hashStorage = new Map();
     };
 
+    const OK = "OK";
+    const NIL = "(nil)";
+
+    // public
+    ctor.prototype.OK = OK;
+    ctor.prototype.NIL = NIL;
+
     ctor.prototype.get = function get(key) {
-        return this.hashStorage.get(key);
+        if (this.hashStorage.has(key)) { 
+            return this.hashStorage.get(key);
+        } else {
+            return NIL;
+        }
     };
 
     ctor.prototype.set = function set(key, value) {
-        return this.hashStorage.set(key, value);
+        this.hashStorage.set(key, value);
+        return OK;
     };
 
-    ctor.prototype.del = function del(key) {
-        if (this.hashStorage.has(key)) {
-            return this.hashStorage.delete(key);
-        } else {
-            throw new errors.CannotDeleteInexistentKey(key);
-        }
+    ctor.prototype.del = function del(...keys) {
+        const result = keys.reduce((accumulator, key) => {
+            if (this.hashStorage.delete(key)) {
+                return accumulator + 1;
+            } else {
+                return accumulator;
+            }
+        }, 0);
+        return result;
     };
 
     return ctor;
 }());
 
-module.exports = muninLib;
+module.exports = Munin;
--- munin-lib/tests/hashFunctions.js
+++ munin-lib/tests/hashFunctions.js
@@ -1,37 +1,38 @@
 import test from 'ava';
-import munin from '../src/munin';
+import Munin from '../src/munin';
 
 test.beforeEach(t => {
-    t.context.db = new munin();
+    console.log('creating new instance of db');
+    t.context.db = new Munin();
 });
 
 const key = 'test-key';
 const value = 'yay!';
 
-test('get returns nil when key is not set', t => {
+test.serial('get returns nil when key is not set', t => {
     const result = t.context.db.get(key);
-    t.is(result, undefined);
+    t.is(result, t.context.db.NIL);
 });
 
-test('set appropriately changes the value of a key', t => {
+test.serial('set appropriately changes the value of a key', t => {
     t.context.db.set(key, value);
     const result = t.context.db.get(key);
     t.is(result, value);
 });
 
-test('del appropriately removes a key', t => {
+test.serial('del appropriately removes a key', t => {
     const result0 = t.context.db.get(key);
-    t.is(result0, undefined);
+    t.is(result0, t.context.db.NIL);
     t.context.db.set(key, value);
     const result1 = t.context.db.get(key);
     t.is(result1, value);
-    t.context.db.del(key);
+    const delResult = t.context.db.del(key);
+    t.is(delResult, 1);
     const result2 = t.context.db.get(key);
-    t.is(result2, undefined);
+    t.is(result2, t.context.db.NIL);
 });
 
-test('del can\'t remove a non-existent key', t => {
-    t.throws(() => {
-        t.context.db.del(key);
-    });
+test.serial('del can\'t remove a non-existent key', t => {
+    const delResult = t.context.db.del(key);
+    t.is(delResult, 0);
 });
